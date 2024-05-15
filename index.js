6fbf4e783b3c82b7d701f49fb5dcfa2cf06d3de8
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2024-05-15 11:45:55 +0600

    added GET/route

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const PRIVATE_APP_ACCESS = process.env.APP_ACCESS_TOKEN;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/deals', async (req, res) => {
+    const contacts = 'https://api.hubapi.com/crm/v3/objects/deals';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Deals | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -22,7 +36,51 @@ const PRIVATE_APP_ACCESS = process.env.APP_ACCESS_TOKEN;
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
+app.post('/update-cobj', async (req, res) => {
+    if (req.query.id) {
+        const objectId = req.query.id;
+        const url = `https://api.hubspot.com/crm/v3/objects/deals/${objectId}`;
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        };
+        const update = {
+            properties: {
+                name: req.body.name,
+                bio: req.body.bio,
+                description: req.body.description
+            }
+        };
+        try {
+            await axios.patch(url, update, { headers });
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error updating custom object");
+        }
+    } else {
+        // no existing object ID, create a new object
+        const url = 'https://api.hubspot.com/crm/v3/objects/pets';
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        };
+        const newObject = {
+            properties: {
+                name: req.body.name,
+                bio: req.body.bio,
+                description: req.body.description,
+            }
+        };
+        try {
+            await axios.post(url, newObject, { headers });
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error creating custom object");
+        }
+    }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
